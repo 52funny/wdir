1c1ceab4afa34a4dd145dc9a783f646960a043d7
52funny <wwq9977@gmail.com>
2021-02-16 10:14:37 +0800

    change print net address

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
+	"net"
 	"path/filepath"
 
 	"github.com/52funny/wdir/config"
@@ -35,9 +37,29 @@ func main() {
 	}
 
 	handler := controller.HandleFastHTTP(config.Path, t, config.Template)
-	utils.Log.Printf("Listen on http://localhost:%v\n", config.Port)
-	if err := fasthttp.ListenAndServe(":"+config.Port, handler); err != nil {
+	address := getNetAddress()
+	fmt.Println("You can now view list in the browser.")
+	fmt.Printf("  Local:%10c  http://localhost:%v\n", ' ', config.Port)
+	for _, addr := range address {
+		fmt.Printf("  On Your NetWork:  http://%v:%v\n", addr, config.Port)
+	}
+	if err := fasthttp.ListenAndServe("0.0.0.0:"+config.Port, handler); err != nil {
 		utils.Log.Println(err)
 		panic(err)
 	}
+}
+
+func getNetAddress() []string {
+	netS := make([]string, 0)
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+	}
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				netS = append(netS, ipNet.IP.String())
+			}
+		}
+	}
+	return netS
 }
